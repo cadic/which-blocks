3f7989867456aece8f0ca0b89f510ee22aad2c6c
Max Lyuchin <maxim.lyuchin@gmail.com>
2023-02-06 12:48:21 +0300

    Added 10up-toolkit

--- tests/cypress/e2e/which-blocks.test.js
+++ tests/cypress/e2e/which-blocks.test.js
@@ -1,36 +1,36 @@
-describe("Basic tests", () => {
-  before(() => {
-    cy.login();
-  });
+describe('Basic tests', () => {
+	before(() => {
+		cy.login();
+	});
 
-  it("Make sure the plugin is active", () => {
-    cy.deactivatePlugin("which-blocks");
-    cy.activatePlugin("which-blocks");
-  });
+	it('Make sure the plugin is active', () => {
+		cy.deactivatePlugin('which-blocks');
+		cy.activatePlugin('which-blocks');
+	});
 
-  it("Should see menu link", () => {
-    cy.visit("/wp-admin/tools.php");
-    cy.get("#menu-tools li a").contains("Which Blocks");
-  });
+	it('Should see menu link', () => {
+		cy.visit('/wp-admin/tools.php');
+		cy.get('#menu-tools li a').contains('Which Blocks');
+	});
 
-  it("Should read blocks usage", () => {
-    cy.visit("/wp-admin/tools.php?page=which-blocks");
+	it('Should read blocks usage', () => {
+		cy.visit('/wp-admin/tools.php?page=which-blocks');
 
-    const expected = {
-      "core/paragraph": 3,
-      "core/heading": 2,
-      "core/quote": 1,
-      "core/buttons": 1,
-      "core/list": 1,
-      "core/button": 1
-    };
+		const expected = {
+			'core/paragraph': 3,
+			'core/heading': 2,
+			'core/quote': 1,
+			'core/buttons': 1,
+			'core/list': 1,
+			'core/button': 1,
+		};
 
-    for (const [key, value] of Object.entries(expected)) {
-      cy.get("td.name")
-        .contains(new RegExp(key))
-        .closest("tr")
-        .find(".usage")
-        .should("contain.text", value);
-    }
-  });
+		for (const [key, value] of Object.entries(expected)) {
+			cy.get('td.name')
+				.contains(new RegExp(key))
+				.closest('tr')
+				.find('.usage')
+				.should('contain.text', value);
+		}
+	});
 });
