ea533b56d08f7f37893c612fbf579efb11f0494d
Max Lyuchin <maxim.lyuchin@gmail.com>
2023-09-22 10:04:37 +0300

    upgrade node dependencies, fix tests, add e2e test reports

--- tests/cypress/config.js
+++ tests/cypress/config.js
@@ -1,15 +1,39 @@
-const { defineConfig } = require('cypress');
-const { readConfig } = require('@wordpress/env/lib/config');
+const { defineConfig } = require("cypress");
+const { readConfig } = require("@wordpress/env/lib/config");
+
+module.exports = defineConfig({
+	chromeWebSecurity: false,
+	fixturesFolder: "tests/cypress/fixtures",
+	screenshotsFolder: "tests/cypress/screenshots",
+	videosFolder: "tests/cypress/videos",
+	downloadsFolder: "tests/cypress/downloads",
+	video: false,
+	e2e: {
+		setupNodeEvents(on, config) {
+			return setBaseUrl(on, config);
+		},
+		specPattern: "tests/cypress/e2e/**/*.test.{js,jsx,ts,tsx}",
+		supportFile: "tests/cypress/support/e2e.js",
+	},
+	reporter: "mochawesome",
+	reporterOptions: {
+		reportFilename: "mochawesome-[name]",
+		reportDir: "tests/cypress/reports",
+		overwrite: false,
+		html: false,
+		json: true,
+	},
+});
 
 /**
  * Set WP URL as baseUrl in Cypress config.
  *
  * @param {Function} on    function that used to register listeners on various events.
  * @param {object} config  Cypress Config object.
- * @returns {object} config Updated Cypress Config object.
+ * @returns config Updated Cypress Config object.
  */
 const setBaseUrl = async (on, config) => {
-	const wpEnvConfig = await readConfig('wp-env');
+	const wpEnvConfig = await readConfig("wp-env");
 
 	if (wpEnvConfig) {
 		const port = wpEnvConfig.env.tests.port || null;
@@ -21,18 +45,3 @@ const setBaseUrl = async (on, config) => {
 
 	return config;
 };
-
-module.exports = defineConfig({
-	fixturesFolder: 'tests/cypress/fixtures',
-	screenshotsFolder: 'tests/cypress/screenshots',
-	videosFolder: 'tests/cypress/videos',
-	downloadsFolder: 'tests/cypress/downloads',
-	video: false,
-	e2e: {
-		setupNodeEvents(on, config) {
-			return setBaseUrl(on, config);
-		},
-		specPattern: 'tests/cypress/e2e/**/*.test.{js,jsx,ts,tsx}',
-		supportFile: 'tests/cypress/support/e2e.js',
-	},
-});
